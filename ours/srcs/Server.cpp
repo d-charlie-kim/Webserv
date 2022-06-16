5b426c2f442fde669bfec2c4e2d5884199537397
Charlie <charlie@Charlie-Mac.local>
2022-06-16 14:49:55 +0900

    [fix] : modified parsing structure and merge with Develop branch

---
+++ ours/srcs/Server.cpp
@@ -0,0 +1,28 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "../includes/Server.hpp"
+#include "../includes/Config.hpp"
+
+Location::Location()
+	: auto_index(false),
+		methods(0x01),
+		client_max_body_size(256),
+		p_error_page(std::make_pair(std::vector<int>(1, 404), std::string("/error_page_404.html"))) {}
+
+Location::Location(const struct Location& default_location)
+	: auto_index(default_location.auto_index),
+		methods(default_location.methods),
+		client_max_body_size(default_location.client_max_body_size),
+		p_error_page(default_location.p_error_page),
+		root(default_location.root),
+		route(default_location.route),
+		v_index(default_location.v_index),
+		p_return(default_location.p_return),
+		cgi(default_location.cgi) {}
+
+Location::~Location() {}
+
+Server::Server() : listen(std::make_pair(std::string("*"), 80)) {}
+
+Server::~Server() {}
+
