6dda6f3a604ca796f93f80492abb13fb73487d7e
sujikim <sujikim@student.42seoul.kr>
2022-07-03 21:26:37 +0900

    [Fix] fix parsing content-length when unchunk body

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,7 +1,7 @@
 #include "Request.hpp"
 
 Request::Request() : method(0), url(""), path(""), location(nullptr), is_cgi(0), is_directory(0),
-					connection(""), content_length(0), content_type(""), body(""), status_code(0) {}
+					connection(""), content_length(0), content_type(""), is_chunk_body(0), body(""), status_code(0) {}
 
 Request& Request::operator=(Request& rq)
 {
@@ -14,6 +14,7 @@ Request& Request::operator=(Request& rq)
 	connection = rq.connection;
 	content_length = rq.content_length;
 	content_type = rq.content_type;
+	is_chunk_body = rq.is_chunk_body;
 	body = rq.body;
 	status_code = rq.status_code;
 	return (*this);
--- srcs/Request_parser.cpp
+++ srcs/Request_parser.cpp
@@ -9,12 +9,12 @@ static int		parse_method(std::string str)
 {
 	int	method = 0;
 
-	const char *methods[9] = {"GET", "POST", "DELETE", "HEAD", "PUT", "OPTIONS", "PATCH", "CONNECT", "TRACE"};
+	const char *methods[3] = {"GET", "POST", "DELETE"};
 	int bit = 0x01;
-	for(int i = 0; i < 9; i++)
+	for(int i = 0; i < 3; i++)
 	{
 		if (methods[i] == str)
-			method |= bit;
+			method = bit;
 		bit <<= 1;
 	}
 	return (method);
@@ -37,18 +37,15 @@ static Location* search_location_in_list(std::string& str, std::vector<Location>
 
 Location* Request_parser::search_location(std::string str, std::vector<Location> &list)
 {
-	// 로케이션 완전일치 확인
 	Location *location = search_location_in_list(str, list);
 	std::string tmp_str(str);
 
-	// 최상위 루트까지 상위 디렉토리 하나씩 일치 확인	
 	while (!location && tmp_str != "/")
 	{
 		tmp_str = tmp_str.substr(0, tmp_str.rfind("/"));
 		location = search_location_in_list(tmp_str, list);
 	}
 
-	// cgi 확인
 	size_t pos = str.find('.');
 	if (location->cgi.size() && pos != std::string::npos && str.substr(pos) == location->cgi)
 		request.is_cgi = true;
@@ -113,7 +110,7 @@ std::list<std::string>		split_line(std::string	origin)
 size_t		unchunk_data(std::list<std::string> list, std::string& body, int &status_code)
 {
 	size_t			length_size;
-	while(!list.empty())
+	while(list.size() > 1)
 	{
 		std::stringstream	iss(list.front());
 		list.pop_front();
@@ -131,9 +128,9 @@ void		Request_parser::m_unchunk_after_body_clear(bool& is_enough_body_length)
 	std::list<std::string>		list = split_line(request.body);
 	request.body.clear();
 	size_t last_length = unchunk_data(list, request.body, request.status_code);
-	
 	if (last_length != 0)
 		is_enough_body_length = false;
+	request.content_length = request.body.size();
 }
 
 void	Request_parser::parse_request(Client& client)
@@ -157,7 +154,6 @@ std::string		transform_tolower(std::string str)
 void	Request_parser::m_parse_request_header (Client& client)
 {
 	Server *server = client.server;
-	// url 입력값 없어도 괜찮도록 디폴트 로케이션 설정
 	Location	*location = &(server->default_location);
 	request.url = "/";
 	request.path = "/";
@@ -169,18 +165,14 @@ void	Request_parser::m_parse_request_header (Client& client)
 	__l_line = __l_file.front();
 	if (!__l_line.size())
 	{
-		// 첫줄이 빈 줄이면 에러 response 최소 메소드는 들어와야 함 400 Bad Request
 		request.status_code = 400;
 		return ;
 	}
-	// 처음 등장하는 단어(=method) 저장, 로케이션 확정 후 메소드 해석할 예정
 	std::string		method_str = __l_line.front();
 	__l_line.pop_front();
 
-	// parse location part
 	if (__l_line.size())
 	{
-		// set location & url & path
 		request.url = __l_line.front();
 		if (request.url.find(":") != std::string::npos)
 		{
@@ -193,14 +185,12 @@ void	Request_parser::m_parse_request_header (Client& client)
 	}
 	request.location = location;
 
-	// 디렉토리 여부 확인 
 	struct stat		status;
 	std::string		path = request.location->root + request.path;
 	stat(path.c_str(), &status);
 	if (S_ISDIR(status.st_mode))
 		request.is_directory = true;
 	
-	// 본격적인 메소드 해석
 	int method = parse_method(method_str);
 	if (!method)
 	{
@@ -209,21 +199,16 @@ void	Request_parser::m_parse_request_header (Client& client)
 	}
 	if (!(method & location->methods))
 	{
-		// 405 not allowed method
 		request.status_code = 405;
 		return ;
 	}
 	request.method = method;
-	// 프로토콜 들어오지 않아도 이상없음
-	// 하지만 들어왔다면 HTTP/1.1과 정확히 일치해야 아니면 505 HTTP Version Not Supported
 	if (!__l_line.empty() && !(__l_line.front() == "HTTP/1.1"))
 	{
 		request.status_code = 505;
 		return ;
 	}
-	// post 메소드라면 반드시 content-length 헤더를 가져야 함
 	bool post_must_have_content_length = !(request.method & POST);
-	// post 메소드가 아니면 항상 true
 	bool is_enough_body_length = true;
 	
 	while (__l_file.size() > 1)
@@ -232,7 +217,6 @@ void	Request_parser::m_parse_request_header (Client& client)
 		if (transform_tolower(__l_line.front()) == "content-length:" && __l_line.size() == 2)
 		{
 			post_must_have_content_length = true;
-			// GET 일때 Content-Length가 들어와도 nginx 200 확인함
 			request.content_length = std::atoi(__l_line.back().c_str());
 			if(request.content_length > static_cast<int>(request.body.size()))
 				is_enough_body_length = false;
@@ -264,33 +248,46 @@ void	Request_parser::m_parse_request_header (Client& client)
 	if (!post_must_have_content_length)
 		request.status_code = 411;
 	if (is_enough_body_length)
-		client._stage = SET_RESOURCE; // FLAG처리 이상이 있으면 stage그대로 없으면 변경 3번째거로로 SET_RESOURCE
+		client._stage = SET_RESOURCE; 
 }
+
+void split_line2(std::string	origin, std::list<std::string>& list)
+{
+	size_t			start = origin.find("\r\n\r\n") + 4;
+	size_t 			pos = origin.find("\r\n", start);
+
+	while (start != std::string::npos && pos != std::string::npos)
+	{
+		std::string		line = origin.substr(start, pos - start);
+		list.push_back(line);
+		start = pos + 2;
+		pos = origin.find("\r\n", start);
+	}
+}
+
 
 void request_msg_parsing(Client& client)
 {
 	if(client.rq.content_length)
 	{
-		size_t			body_start_pos = client.request_msg.find("\r\n\r\n") + 4;
-		std::string		body = client.request_msg.substr(body_start_pos);
-		
 		if (client.rq.is_chunk_body)
 		{
-			std::list<std::string> list = split_line(body);
+			std::list<std::string> list;
+			split_line2(client.request_msg, list);
 			client.rq.body.clear();
 			size_t last_length = unchunk_data(list, client.rq.body, client.rq.status_code);
 			if (client.rq.status_code || last_length == 0)
 				client._stage = SET_RESOURCE;
+			client.rq.content_length = client.rq.body.size();
 			return ;
 		}
-		client.rq.body = body;
-		if (body.size() >= static_cast<size_t>(client.rq.content_length))
+		client.rq.body = client.request_msg.substr(client.request_msg.find("\r\n\r\n") + 4);
+		if (client.rq.body.size() >= static_cast<size_t>(client.rq.content_length))
 			client._stage = SET_RESOURCE;
-		if (body.size() > static_cast<size_t>(client.rq.content_length))
-			client.rq.body = body.substr(0, client.rq.content_length);
+		if (client.rq.body.size() > static_cast<size_t>(client.rq.content_length))
+			client.rq.body = client.rq.body.substr(0, client.rq.content_length);
 		return ;
 	}
-	// 초기 파싱
 	Request_parser		parser(client.request_msg);
 	parser.parse_request(client);
 	client.rq = parser.request;
